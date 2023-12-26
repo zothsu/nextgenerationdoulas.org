75dda6ed9e904d78fde42c29c0cc1360fc24532d
Zoe Sullivan <0442338+zothsu@users.noreply.github.com.>
2023-12-26 14:22:56 -0800

    create v1 of mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,29 @@
-let boardMembers = {
-  
+const navBtn = document.querySelector("#nav-btn");
+const navContainer = document.querySelector("#nav-container");
+
+navBtn.addEventListener("click", function () {
+  navContainer.classList.toggle("appear");
+
+  if (navContainer.classList.contains("appear")) {
+    menuIsOpen();
+  } else {
+    menuIsClosed();
+  }
+});
+
+document.addEventListener("click", function (e) {
+  if (!navContainer.contains(e.target) && e.target !== navBtn) {
+    navContainer.classList.remove("appear");
+    menuIsClosed();
+  }
+});
+
+function menuIsClosed() {
+  navBtn.classList.add("fa-bars");
+  navBtn.classList.remove("fa-xmark");
+}
+
+function menuIsOpen() {
+  navBtn.classList.remove("fa-bars");
+  navBtn.classList.add("fa-xmark");
 }
